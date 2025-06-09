feb899db6c386ff2068f07874d9d5d233845a9e1
AhmedBedeir <bederahmed0@gmail.com>
2025-06-09 14:15:00 +0300

    initial config

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-primary p-4 font-bold text-center">Welcome to My Blog App</h1>
+    </div>
+  );
+}
+
+export default App;
